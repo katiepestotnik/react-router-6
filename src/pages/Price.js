3f7f0f4a946a7be1b80ab9118bff02b141b8eef4
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-05-14 15:24:22 -0600

    add dependency array

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,4 +1,40 @@
-const Price = ()=>{
-    return(<h1>Price</h1>)
+//hook returns object with params after the colon
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react'
+
+const Price = () => {
+    const APIKEY = "pk_1a40cb0759a945dda62b0217bb01ac6f"
+    // grabbing currency symbol with object destructure
+    const { symbol } = useParams()
+    const url = `https://cloud.iexapis.com/stable/crypto/${symbol}/price?token=${APIKEY}`
+    
+    const [coin, setCoin] = useState(null)
+    const [update, setUpdate]= useState(0)
+    const getCoin = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        setCoin(data)
+
+    }
+    useEffect(() => {
+        getCoin()
+
+        setTimeout(() => { setUpdate(update + 1) }, 5000)
+        console.log(coin)
+    },[update])
+    const loaded = () => {
+        return (
+            <>
+                <h1>Symbol: {coin.symbol}</h1>
+                <h3>Price: {coin.price}</h3>
+            </>
+
+        )
+    }
+    const loading = () => {
+        return<h1>Loading...</h1>
+    }
+    return(    coin?loaded():loading())
+
 }
 export default Price
